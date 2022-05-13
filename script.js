3d028976da85493f3f2e57f94396359351292ccc
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-05-13 11:48:45 +0300

    notification
    Button for adding notifications for facebook messenger

---
+++ script.js
@@ -0,0 +1,11 @@
+const notify = document.querySelector('.notify');
+const btn = document.querySelector('.btn');
+const add = addEventListener('click', active);
+
+function active() {
+	let add = Number(notify.getAttribute('data-count') || 0);
+	notify.setAttribute('data-count', add + 1);
+	if(add === 0) {
+		notify.classList.add('add-number');
+	}
+}
