a73618248f11ed8ba900dd83cb23307b23964c27
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-05-13 16:39:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const notify = document.querySelector('.notify');
 const btn = document.querySelector('.btn');
-const add = addEventListener('click', active);
+let add = addEventListener('click', active);
 
 function active() {
 	let add = Number(notify.getAttribute('data-count') || 0);
-	notify.setAttribute('data-count', add + 1);
 	if(add === 0) {
 		notify.classList.add('add-number');
+	}
+	if(add !== 99) {
+		notify.setAttribute('data-count', add + 1);
 	}
 }
